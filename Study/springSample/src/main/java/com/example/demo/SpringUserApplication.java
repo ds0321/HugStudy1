eaa774665ad957b35f34b43d2f81ac2bcd52f3b8
ds0321 <159526458+ds0321@users.noreply.github.com>
2024-03-21 22:36:12 +0900

    4-16

--- Study/springSample/src/main/java/com/example/demo/SpringUserApplication.java
+++ Study/springSample/src/main/java/com/example/demo/SpringUserApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSampleApplication {
+public class SpringUserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSampleApplication.class, args);
+		SpringApplication.run(SpringUserApplication.class, args);
 	}
 
 }
--- Study/springSample/src/main/java/com/example/demo/repository/UserRepository.java
+++ Study/springSample/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,13 +1,15 @@
 package com.example.demo.repository;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.demo.entity.SubjectEntity;
+import com.example.demo.entity.UserEntity;
+
 
 /**
- * 科目情報 Repository
+ * ユーザー情報 Repository
  */
 @Repository
-public interface SubjectRepository extends JpaRepository<SubjectEntity, Integer> {
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 }
